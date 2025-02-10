8ec7ffb3f7cc5ac5e32aefbd8d427a67d8a94ab9
ChrissiMaa <christellemaa@yahoo.de>
2025-02-10 22:53:10 +0100

    Initial Commit

---
+++ SmartPantry/SmartPantryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartPantryApp.swift
+//  SmartPantry
+//
+//  Created by Christelle Maaß on 10.02.25.
+//
+
+import SwiftUI
+
+@main
+struct SmartPantryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
